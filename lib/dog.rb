0ff236b081752d0ea3e0f17f8261e251212f4852
nathalia <nathaliatroina@gmail.com>
2022-02-22 13:54:26 -0500

    commit tree 97c5d87e405ebef255f72f23e67648df77617824

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+      end
+    
+      def name
+        @this_dogs_name
+      end
 end
+#com o arroba na frente, e porque queremos usar instance varible
+#ao inves de local varible
+#assim o methodo pode acessar outros methodos
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+
+puts lassie.name 
